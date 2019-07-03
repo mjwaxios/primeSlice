70c8ba98524bc7a37ac691c4be8365e6ce886c5a
mwyrick <mjw@axiosengineering.com>
2019-07-03 10:20:03 -0400

    Renamed to primeSlice including package Name

--- primeSlice.go
+++ primeSlice.go
@@ -1,14 +1,11 @@
 // Package prime_dns provides a way to generate a slice of prime numbers
-package prime_dns
+package primeSlice
 
-// GetSlice will return a slice containing all the prime numbers
-// from 2 to limit.
+// GetSlice will return a slice containing the prime numbers from 2 to limit
 // It returns the slice
 func GetSlice(limit int) []int {
 	limit = limit + 1
-	// creates a slice of false with length of limit
 	bools := make([]bool, limit)
-	// implies up to the sqrt of limit
 	for k := 2; k*k <= limit; k++ {
 		if bools[k] != true {
 			for ix := k * k; ix < limit; ix += k {
@@ -16,7 +13,6 @@ func GetSlice(limit int) []int {
 			}
 		}
 	}
-	// index of remaining False in bools = a prime number
 	primes := []int{}
 	for ix := 2; ix < limit; ix++ {
 		if bools[ix] != true {
